9026f6d395d07c224e7192e6b71928f8743bff2f
AgbogidiRhunor <roobill1234@gmail.com>
2026-04-15 22:46:47 +0100

    update: updated requirements.txt for deployment fix(core/urls.py): accounts app was added

--- core/urls.py
+++ core/urls.py
@@ -5,11 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Move accounts to root so landing page is accessible at '/'
     path('', include('apps.accounts.urls', namespace='accounts')),
-    
-    # Keep these under their respective paths
+    path('accounts/', include('apps.accounts.urls', namespace='accounts')),
     path('notes/', include('apps.notes.urls', namespace='notes')),
     path('workspaces/', include('apps.workspaces.urls', namespace='workspaces')),
 ]
